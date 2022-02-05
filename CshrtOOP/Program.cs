230a11b16dcfc56a83ac91a2aba9d77a6a181d81
Philip Wahlbom <philip@wahlbom.net>
2022-02-05 17:43:27 -0500

    Update Program.cs

--- CshrtOOP/Program.cs
+++ CshrtOOP/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace CshrtOOP
 {
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //this is the constructor
+        public Customer(string firstName = "Insert first name", string lastName = "Insert last name") {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            var cust = new Customer();
+            Console.WriteLine(string.Format("First Name : {0}\nLast Name : {1}", cust.FirstName, cust.LastName));
+            Console.ReadLine();
         }
     }
 }
